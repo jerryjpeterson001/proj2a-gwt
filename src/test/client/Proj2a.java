b74c1a055fbe3f43d9938a56ec789df124a216ad
jerryjp <jerryjp@ubuntu.(none)>
2012-03-01 15:24:32 -1000

    Select and Delete

--- src/test/client/Proj2a.java
+++ src/test/client/Proj2a.java
@@ -18,6 +18,8 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SingleSelectionModel;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -25,9 +27,12 @@ public class Proj2a implements EntryPoint, ClickHandler
 {
    VerticalPanel mainPanel = new VerticalPanel();
    String baseURL = "http://localhost:3000";
-   ArrayList<MyStudent> students =
-      new ArrayList<MyStudent>();
+   ArrayList<MyStudent> students;
+      //new ArrayList<MyStudent>();
    JsArray<Student> jsonData;
+   Button addButton = new Button("Add");
+   Button deleteButton = new Button("Delete");
+   MyStudent selectedStudent = null;
    Button addStudentButton = new Button("Add Student");
    TextBox fnBox = new TextBox();
    TextBox lnBox = new TextBox();
@@ -50,11 +55,13 @@ public class Proj2a implements EntryPoint, ClickHandler
    }
    public void onModuleLoad()
    {
-      //String url = baseURL + "/students/index.json";
-      //getRequest(url,"getStudents");
+      String url = baseURL + "/students/index.json";
+      getRequest(url,"getStudents");
+      addButton.addClickHandler(this);
+      deleteButton.addClickHandler(this);
 	  addStudentButton.addClickHandler(this); 
       RootPanel.get().add(mainPanel);
-      setupAddStudent();
+      //setupAddStudent();
    }
    public void onClick(ClickEvent e)
    {
@@ -67,7 +74,18 @@ public class Proj2a implements EntryPoint, ClickHandler
 			  URL.encode(lnBox.getText().trim()) + "&" +
 			  URL.encode("major") + "=" +
 			  URL.encode(majBox.getText().trim());
+		   fnBox.setText("");
+		   lnBox.setText("");
+		   majBox.setText("");
 		   postRequest(url,postData,"postStudent");
+	   }
+	   else if (source == addButton) {
+		   setupAddStudent();
+	   }
+	   else if (source == deleteButton) {
+		   String url = baseURL + "/students/deleteStudent";
+		   String postData = URL.encode("student_id") + "=" + URL.encode("" + selectedStudent.id);
+		   postRequest(url,postData,"deleteStudent");
 	   }
    }
    public void getRequest(String url, final String getType) {
@@ -107,7 +125,7 @@ public class Proj2a implements EntryPoint, ClickHandler
 			   }
 			   public void onResponseReceived(final Request request, final Response response)
 			   {
-				   if (postType.equals("postStudent")) {
+				   if (postType.equals("postStudent") || postType.equals("deleteStudent")) {
 					   mainPanel.clear();
 					   String url = baseURL + "/students/index.json";
 					   getRequest(url,"getStudents");
@@ -121,7 +139,9 @@ public class Proj2a implements EntryPoint, ClickHandler
    } // end postRequest()
    private void showStudents(String responseText)
    {
-      jsonData = getData(responseText);
+      //mainPanel.clear();
+	  jsonData = getData(responseText);
+	  students = new ArrayList<MyStudent>();
       Student student = null;
       for (int i = 0; i < jsonData.length(); i++) {
          student = jsonData.get(i);
@@ -148,15 +168,43 @@ public class Proj2a implements EntryPoint, ClickHandler
                return student.last_name;
             }
          };
-      
+      TextColumn<MyStudent> majCol =
+    	  new TextColumn<MyStudent>()
+    	  {
+    	  	@Override
+    	  	public String getValue(MyStudent student)
+    	  	{
+    	  		return student.major;
+    	  	}
+    	  };
+      final SingleSelectionModel<MyStudent> selectionModel =
+    	 new SingleSelectionModel<MyStudent>();
+      table.setSelectionModel(selectionModel);
+      selectionModel.addSelectionChangeHandler(
+    	 new SelectionChangeEvent.Handler()
+    	 {
+    		 public void onSelectionChange(SelectionChangeEvent e)
+    		 {
+    			 MyStudent selected = selectionModel.getSelectedObject();
+    			 if (selected != null) {
+    				 selectedStudent = selected;
+    			 }
+    		 }
+    	 });
       table.addColumn(fnameCol, "First Name");
       table.addColumn(lnameCol, "Last Name");
+      table.addColumn(majCol, "Major");
       table.setRowCount(students.size(),true);
       table.setRowData(0,students);
+      HorizontalPanel buttonRow = new HorizontalPanel();
+      mainPanel.add(addButton);
+      mainPanel.add(deleteButton);
+      mainPanel.add(buttonRow);
       mainPanel.add(table);
    } // end showStudents()
    private void setupAddStudent()
    {
+	   mainPanel.clear();
 	   VerticalPanel addStudentPanel = new VerticalPanel();
 	   Label fnLabel = new Label("First Name");
 	   HorizontalPanel fnRow = new HorizontalPanel();
